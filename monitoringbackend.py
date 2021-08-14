77e7b81abe13fe49e262f8049871761f232a622a
YarosSem <yar.chkin@yandex.ru>
2021-08-14 13:42:26 +0300

    В monitoringbackent.py добавлена функция возвращающая координаты точек температуры и линий деления на дни

--- monitoringbackend.py
+++ monitoringbackend.py
@@ -3,6 +3,7 @@ import mysql.connector
 from mysql.connector import Error, MySQLConnection
 import hashlib
 import re
+import numpy as np
 
 def current_time():
     """"
@@ -160,3 +161,95 @@ def password_is_correct(password):
             return True
         else:
             return False
+
+def current_sickness_figure_points(login_id, dbconfig):
+    """
+    Возвращает:
+        -координаты точек для графика по текущей болезни
+        -координаты дневных делителей по оси абсцисс
+        -координаты дневных делителей по оси ординат
+    """
+    conn = MySQLConnection(**dbconfig)
+    cursor = conn.cursor()
+    
+    # Запрос на данные о болезнях пользователя за последние 30 дней
+    query = """
+            SELECT temperature, measurement_day, measurement_time
+            FROM temperature_measurement
+            WHERE login_id = {login_id} AND DATEDIFF(CURRENT_DATE, measurement_day) < 30
+            ORDER BY measurement_day ASC, measurement_time DESC;
+            """.format(login_id = login_id)
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    # Если запрос ничего не вернул
+    if not rows:
+        conn.close()
+        return None
+    
+    # Заполненние списака, включающего записи по текущей болезни
+    current_sickness_list = []
+    now = datetime.datetime.now()
+    now_date = now.date()
+    if (now_date - rows[-1][1]).days >= 7:  # Если последняя запись была не
+        conn.close()                        # менее недели назад
+        return None
+    current_sickness_list.append(rows[-1])
+    for row in rows[::-1][1:]:
+        if (current_sickness_list[-1][1] - row[1]).days < 6:
+            current_sickness_list.append(row)
+        else:
+            break
+    # Заполнение списка из кортежей, включающих записи дня
+    monitoring_days = tuple(set(row[1] for row in current_sickness_list))
+    #print(monitoring_days)
+    current_sickness_list_days = []
+    index = 0
+    len_csld = len(current_sickness_list)
+    for day in monitoring_days:
+        current_sickness_list_days.append([])
+        for i in range(index, len_csld):
+            row = current_sickness_list[i]
+            if row[1] == day:
+                current_sickness_list_days[-1].append(row)
+            else:
+                index = i
+                break
+    #print(current_sickness_list_days)
+    
+    # Определение координат точек
+    # Координаты дневных делений по оси асбсцисс
+    x_days_points = np.linspace(0, 1000, len(monitoring_days) + 1)
+    # Координаты точек измерения температуры
+    temperature_coords = np.array([[0, 0]])[:-1] # Пустой двумерный массив
+    for i, day in enumerate(current_sickness_list_days[::-1]):
+        day_x_coords = x_days_points[i : i + 2]
+        for row in day:
+            x = day_x_coords[0] + (day_x_coords[1] -day_x_coords[0]) * row[2].seconds / 86400
+            y = float(row[0])
+            temperature_coords = np.vstack((temperature_coords, [x, y]))
+    #print(temperature_coords)
+    # Координаты дневных делений по оси ординат
+    y_days_points = np.array([temperature_coords[:, 1].min(), temperature_coords[:, 1].max()])
+    conn.close()
+    #print(x_days_points[1 : -1])
+    #print(y_days_points)
+    return temperature_coords, x_days_points, y_days_points
+
+def insert_temperature_mesurement_result(temperature, measurement_day,
+                                         measurement_time, login_id, dbconfig):
+    conn = MySQLConnection(**dbconfig)
+    cursor = conn.cursor()
+    
+    insertion_query = """
+                      INSERT INTO temperature_measurement (login_id, temperature,
+                                                           measurement_day, measurement_time)
+                      VALUES ({login_id}, {temperature}, '{measurement_day}', '{measurement_time}');
+                      """.format(login_id = login_id, temperature = temperature,
+                                 measurement_day = measurement_day,
+                                 measurement_time = measurement_time)
+    cursor.execute(insertion_query)
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    current_sickness_figure_points(3, connection_to_base())
